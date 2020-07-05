f2df4938222b6af9a3b5834d2762fd0461daa03b
Gryffydd David <darthmarshie@gmail.com>
2020-07-05 22:33:26 +0200

    Added a bunch of stuff. Can do a drunken walk between two specific points. Can also generate blobs of random noise on edge cells to add some interest (Function is called DrunkenNoise)

--- ProceduralDungeon1/Dungeon.cs
+++ ProceduralDungeon1/Dungeon.cs
@@ -3,19 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Numerics;
 
 namespace ProceduralDungeon1
 {
+    //public struct Vector2
+    //{
+    //    public Vector2(int x, int y)
+    //    {
+    //        X = x;
+    //        Y = y;
+    //    }
+
+    //    public int X { get; }
+    //    public int Y { get; }
+    //}
+
     class Dungeon
     {
         static Random Random = new Random();
-
+        
         bool[,] NoiseMap;
+        char[,] DungeonMap; //Characters to draw the dungeon
+        int StartX, StartY;
+
+        List<Vector2> EdgeList = new List<Vector2>();
+
+
 
         public void GenerateNoiseMap(int xSize, int ySize, int threshold)
         {
             #region Fill NoiseMap with solid blocks
             NoiseMap = new bool[xSize, ySize];
+            DungeonMap = new char[xSize, ySize];
+
             for (int x = 0; x < NoiseMap.GetLength(0); x++)
             {
                 for (int y = 0; y < NoiseMap.GetLength(1); y++)
@@ -46,6 +67,7 @@ namespace ProceduralDungeon1
         {
             #region Fill NoiseMap with solid blocks
             NoiseMap = new bool[xSize, ySize];
+            DungeonMap = new char[xSize, ySize];
             for (int x = 0; x < NoiseMap.GetLength(0); x++)
             {
                 for (int y = 0; y < NoiseMap.GetLength(1); y++)
@@ -56,9 +78,8 @@ namespace ProceduralDungeon1
             #endregion
 
             //Pick a random starting position
-            int StartX = Random.Next(0, xSize-1);
-            int StartY = Random.Next(0, ySize-1);
-            NoiseMap[StartX, StartY] = false;
+            StartX = Random.Next(0, xSize/2);
+            StartY = Random.Next(0, ySize/2);
 
             //Percentage of cells that should be floor
             int floorCells = ((xSize * ySize) / 100) * threshold;
@@ -66,35 +87,71 @@ namespace ProceduralDungeon1
             //Direction to move randomly
             int xChange = 0;
             int yChange = 0;
-            
+
+            int CurrentX = StartX;
+            int CurrentY = StartY;
+            float yBias, xBias;
+            yBias = 0;
+            xBias = 0;
+
             do
             {
-                xChange = Random.Next(-1, 2);
-                yChange = Random.Next(-1, 2);
+                float currentXPerc = ((float)CurrentX / (float)xSize) * 100f;
+                float currentYPerc = ((float)CurrentY / (float)ySize) * 100f;
+
+                if (currentXPerc < 25)
+                    xBias += 0.005f;
+                else if (currentXPerc > 75)
+                    xBias -= 0.005f;
+
+                if (currentYPerc < 25)
+                    yBias += 0.005f;
+                else if (currentYPerc > 75)
+                    yBias -= 0.005f;
+
+                #region X Direction
+                if (RandomPercent() > 50 + xBias)
+                    xChange = -1;
+                else
+                    xChange = 1;
+                #endregion
+
+                #region Y Direction
+                if (RandomPercent() > 50 + yBias)
+                    yChange = -1;
+                else
+                    yChange = 1;
+                #endregion
 
                 #region Limit to 4 cardinal directions
                 if (Random.NextDouble() < 0.5f)
                     xChange = 0;
                 else
-                    yChange = 0; 
+                    yChange = 0;
                 #endregion
 
-                int NextX = Clamp(StartX + xChange, 0, xSize-1);
-                int NextY = Clamp(StartY + yChange, 0, ySize-1);
+                int NextX = Clamp(CurrentX + xChange, 0, xSize-1);
+                int NextY = Clamp(CurrentY + yChange, 0, ySize-1);
 
                 if (NoiseMap[NextX, NextY] == true)
                 {                    
                     NoiseMap[NextX, NextY] = false;
                     floorCells--;
+
+                    //System.Threading.Thread.Sleep(20);
+                    //Console.Clear();
+                    //DrawNoiseMap();
                 }
 
-                StartX = NextX;
-                StartY = NextY;
+                CurrentX = NextX;
+                CurrentY = NextY;
             }
             while (floorCells > 0);
+
+            
         }
 
-        public void SmoothDungeon()
+        public void SmoothDungeon(int thresh)
         {
             for (int x = 0; x < NoiseMap.GetLength(0); x++)
             {
@@ -120,7 +177,7 @@ namespace ProceduralDungeon1
                     }
 
                     //Cell is dead and 6 or more cells around it are alive. Make this cell alive.
-                    if (alive == false && aliveCount >= 6)
+                    if (alive == false && aliveCount >= thresh)
                     {
                         NoiseMap[x, y] = true;                        
                     }
@@ -136,38 +193,39 @@ namespace ProceduralDungeon1
 
         public void FindEdges()
         {
-            //for (int y = 0; y < NoiseMap.GetLength(0); y++)
-            //{
-            //    for (int x = 0; x < NoiseMap.GetLength(1); x++)
-            //    {
-            //        bool alive = NoiseMap[y, x];
-            //        int deadCount = 0;
+            for (int x = 0; x < NoiseMap.GetLength(0); x++)
+            {
+                for (int y = 0; y < NoiseMap.GetLength(1); y++)
+                {
+                    bool alive = NoiseMap[x, y];
+                    int deadCount = 0;
 
-            //        //Count dead neighbours
-            //        for (int yTest = -1; yTest < 2; yTest++)
-            //        {
-            //            for (int xTest = -1; xTest < 2; xTest++)
-            //            {
-            //                if (!(xTest == 0 && yTest == 0))
-            //                {
-            //                    int xClamp = Clamp(x + xTest, 0, NoiseMap.GetLength(1) - 1);
-            //                    int yClamp = Clamp(y + yTest, 0, NoiseMap.GetLength(0) - 1);
+                    //Count dead neighbours
+                    for (int yTest = -1; yTest < 2; yTest++)
+                    {
+                        for (int xTest = -1; xTest < 2; xTest++)
+                        {
+                            if (!(xTest == 0 && yTest == 0))
+                            {
+                                int xClamp = Clamp(x + xTest, 0, NoiseMap.GetLength(0) - 1);
+                                int yClamp = Clamp(y + yTest, 0, NoiseMap.GetLength(1) - 1);
 
-            //                    if (NoiseMap[yClamp, xClamp] == false)
-            //                        deadCount++;
-            //                }
-            //            }
-            //        }
+                                if (NoiseMap[xClamp, yClamp] == false)
+                                    deadCount++;
+                            }
+                        }
+                    }
 
-            //        if (alive == true)
-            //        {
-            //            if (deadCount >= 1)
-            //            {
-            //                DungeonArray[y, x] = '█';
-            //            }
-            //        }
-            //    }
-            //}
+                    if (alive == true)
+                    {
+                        if (deadCount >= 1)
+                        {
+                            DungeonMap[x, y] = 'E';
+                            EdgeList.Add(new Vector2(x, y));
+                        }
+                    }
+                }
+            }
         }
 
         public void DrawNoiseMap()
@@ -181,10 +239,12 @@ namespace ProceduralDungeon1
                     if (NoiseMap[x, y] == true)
                     {
                         yString += '▓';
+                        DungeonMap[x,y] = '▓';
                     }
                     else
                     {
                         yString += ' ';
+                        DungeonMap[x, y] = ' ';
                     }
                 }
 
@@ -192,9 +252,202 @@ namespace ProceduralDungeon1
             }
         }
 
+        public void DrawDungeonMap()
+        {
+            for (int y = 0; y < NoiseMap.GetLength(1); y++)
+            {
+                String yString = "";
+
+                for (int x = 0; x < NoiseMap.GetLength(0); x++)
+                {
+                    yString += DungeonMap[x, y];
+                }
+
+                Console.WriteLine(yString);
+            }
+        }
+
+        public void DrunkenNoise()
+        {
+            //for each cell that isn't part of the original walk
+            //Find it's percentage distance to the nearest edge of the walk
+            //Use that percentage to randomise whether it's alive or not
+            //EdgeList.OrderBy(cell => cell.X + cell.Y > RandomPercent());
+            while (EdgeList.Count > 5)
+            {
+                EdgeList.RemoveRange(Random.Next(0, EdgeList.Count), 1);
+            }
+
+
+            for (int x = 0; x < NoiseMap.GetLength(0); x++)
+            {
+                for (int y = 0; y < NoiseMap.GetLength(1); y++)
+                {
+                    if (NoiseMap[x, y] == true)
+                    {
+                        //Find closest edge
+                        //foreach (Vector2 edgeCell in EdgeList)
+
+                        
+
+                        if (EdgeList.Count > 0)
+                        {
+                            Vector2 edgeCell = EdgeList.OrderBy(cell => GetDist((int)cell.X, (int)cell.Y, x, y)).First();
+                            {
+                                float dist = GetDist(x, y, (int)edgeCell.X, (int)edgeCell.Y);
+
+                                if (dist < 15)
+                                {
+                                    float distPerc = (dist / 15) * 100;
+
+                                    if (RandomPercent() > distPerc)
+                                        NoiseMap[x, y] = false;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public void DrunkenWalkToPoint(Vector2 startPos, Vector2 endPos)
+        {
+            //While currentPos != endPos
+            //Pick a random direction and move in that direction
+            //If the direction is away from endPos
+            //Weight the next move slightly more towards endPos
+            //If the direction is towards endPos. Reset the weights
+            Vector2 currentPos = startPos;
+
+            List<Vector2> WalkPath = new List<Vector2>();
+
+            int xChange = 0;
+            int yChange = 0;
+            float yBias, xBias;
+            yBias = 0;
+            xBias = 0;
+
+            int count = 0;
+
+            do
+            {
+                #region X Direction
+                if (RandomPercent() > 50 + xBias)
+                    xChange = -1;
+                else
+                    xChange = 1;
+                #endregion
+
+                #region Y Direction
+                if (RandomPercent() > 50 + yBias)
+                    yChange = -1;
+                else
+                    yChange = 1;
+                #endregion
+
+                #region Limit to 4 cardinal directions
+                if (Random.NextDouble() < 0.5f)
+                {
+                    xChange = 0;
+                }
+                else
+                {
+                    yChange = 0;
+                }
+                #endregion
+
+                int NextX = Clamp((int)currentPos.X + xChange, 0, NoiseMap.GetLength(0) - 1);
+                int NextY = Clamp((int)currentPos.Y + yChange, 0, NoiseMap.GetLength(1) - 1);
+
+                if (NoiseMap[NextX, NextY] == true)
+                {
+                    //Direction from the current position to the end
+                    Vector2 dirToEnd = endPos - currentPos;
+                    Vector2 vecDelta = Vector2.Normalize(dirToEnd);
+                    dirToEnd = new Vector2(Math.Sign(dirToEnd.X), Math.Sign(dirToEnd.Y));
+
+                    float biasChange;
+
+                    if (Vector2.Distance(endPos, currentPos) < 30)
+                        biasChange = 15f;
+                    else
+                        biasChange = 2f;
+
+                    if (xChange < dirToEnd.X)
+                        xBias += biasChange * Math.Abs(vecDelta.X);
+                    else if (xChange > dirToEnd.X)
+                        xBias -= biasChange * Math.Abs(vecDelta.X);
+                    else if (xChange == dirToEnd.X)
+                        xBias = 0f;
+
+                    if (yChange < dirToEnd.Y)
+                        yBias += biasChange * Math.Abs(vecDelta.Y);
+                    else if (yChange > dirToEnd.Y)
+                        yBias -= biasChange * Math.Abs(vecDelta.Y);
+                    else if (yChange == dirToEnd.Y)
+                        yBias = 0f;
+
+                    NoiseMap[NextX, NextY] = false;
+                    WalkPath.Add(new Vector2(NextX, NextY));
+                    count++;
+                }
+
+                currentPos = new Vector2(NextX, NextY);
+            }
+            while (currentPos != endPos);
+
+            //Widen the path
+            foreach (Vector2 cell in WalkPath)
+            {
+                if (NoiseMap[Clamp((int)cell.X - 1, 0, NoiseMap.GetLength(0) - 1), (int)cell.Y] == true &&
+                    NoiseMap[Clamp((int)cell.X + 1, 0, NoiseMap.GetLength(0) - 1), (int)cell.Y])
+                {
+                    if (RandomPercent() > 50)
+                        NoiseMap[(int)cell.X + 1, (int)cell.Y] = false;
+                    else
+                        NoiseMap[(int)cell.X - 1, (int)cell.Y] = false;
+                }
+
+                if (NoiseMap[(int)cell.X, Clamp((int)cell.Y + 1, 0, NoiseMap.GetLength(1)-1)] == true &&
+                    NoiseMap[(int)cell.X, Clamp((int)cell.Y - 1, 0, NoiseMap.GetLength(1)-1)])
+                {
+                    if (RandomPercent() > 50)
+                        NoiseMap[(int)cell.X, (int)cell.Y + 1] = false;
+                    else
+                        NoiseMap[(int)cell.X, (int)cell.Y - 1] = false;
+                }
+            }
+        }
+
+        public void FillMap(int xSize, int ySize)
+        {
+            #region Fill NoiseMap with solid blocks
+            NoiseMap = new bool[xSize, ySize];
+            DungeonMap = new char[xSize, ySize];
+
+            for (int x = 0; x < NoiseMap.GetLength(0); x++)
+            {
+                for (int y = 0; y < NoiseMap.GetLength(1); y++)
+                {
+                    NoiseMap[x, y] = true;
+                }
+            }
+            #endregion
+        }
+
         public static int Clamp(int value, int min, int max)
         {
             return (value < min) ? min : (value > max) ? max : value;
+        }
+
+        public float RandomPercent()
+        {
+            return (float)Random.NextDouble() * 100;
+        }
+
+        public int GetDist(int x1, int y1, int x2, int y2)
+        {
+            return (int)Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
         }
     }
 }
--- ProceduralDungeon1/Program.cs
+++ ProceduralDungeon1/Program.cs
@@ -3,27 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Numerics;
 
 namespace ProceduralDungeon1
 {
     class Program
     {
+        static Random Random = new Random();
+
         static void Main(string[] args)
         { 
             Dungeon dungeon = new Dungeon();
 
             //dungeon.GenerateNoiseMap(230, 60, 45);
-            dungeon.GenerateDrunkenWalk(230, 60, 45);
+            //dungeon.GenerateDrunkenWalk(230, 60, 10);
+            do
+            {
+                //dungeon.GenerateNoiseMap(230, 60, 45);
+                //dungeon.GenerateDrunkenWalk(230, 60, 30);
+                //dungeon.DrawNoiseMap();
+                //Console.WriteLine(" ");
 
-            dungeon.DrawNoiseMap();
-            Console.WriteLine(" ");
+                //dungeon.FindEdges();
+                //dungeon.DrunkenNoise();
+                dungeon.FillMap(230, 60);
+                dungeon.DrunkenWalkToPoint(new Vector2(0, 0), new Vector2(229, 59));
+                dungeon.DrunkenWalkToPoint(new Vector2(0, 59), new Vector2(229, 0));
+                //dungeon.DrunkenWalkToPoint(new Vector2(0, 50), new Vector2(150, 25));
+                //dungeon.DrunkenWalkToPoint(new Vector2(0, 50), new Vector2(150, 25));
+                dungeon.DrawNoiseMap();
+                Console.WriteLine(" ");
 
-            for (int i = 0; i < 5; i++)
-                dungeon.SmoothDungeon();
+                dungeon.SmoothDungeon(6);
 
-            dungeon.DrawNoiseMap();
+                for (int i = 0; i < 5; i++)
+                    dungeon.SmoothDungeon(5);
 
-            Console.ReadKey();
+                dungeon.SmoothDungeon(6);
+                dungeon.DrawNoiseMap();                
+            }
+            while (Console.ReadKey().Key == ConsoleKey.A);
         }
     }
 }
